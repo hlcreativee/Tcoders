7052990155d22801feaf76d50f48c3b8626911b5
rizalfadeli <h9fadeli03smpn1pace17@gmail.com>
2026-05-05 11:28:06 +0700

    revisi route prediksi

--- web-project/routes/web.php
+++ web-project/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\PrediksiController;
-use App\Http\Controllers\PredictionController;
+
 
 
 // DASHBOARD
@@ -18,7 +18,7 @@ Route::post('/transaksi', [TransaksiController::class, 'store'])->name('transaks
 // PREDIKSI
 Route::get('/prediksi', [PrediksiController::class, 'index'])->name('prediksi');
 Route::prefix('predictions')->group(function () {
-    Route::get('/', [PredictionController::class, 'index'])->name('predictions.index');
-    Route::get('/create', [PredictionController::class, 'create'])->name('predictions.create');
-    Route::post('/store', [PredictionController::class, 'store'])->name('predictions.store');
+    Route::get('/', [PrediksiController::class, 'index'])->name('predictions.index');
+    Route::get('/create', [PrediksiController::class, 'create'])->name('predictions.create');
+    Route::post('/store', [PrediksiController::class, 'store'])->name('predictions.store');
 });
